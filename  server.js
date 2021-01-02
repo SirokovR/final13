74089572b56b4b999e4064ec3fa9ce5b6bc3ef94
laisk <laisk1@hotmail.com>
2021-01-02 18:57:36 +0200

    deploy.3.1

---  server.js
+++  server.js
@@ -6,7 +6,6 @@ const app = express();
 app.use(express.static('./dist/ff13'));
 
 app.get('/*', (req, res) =>
-  res.sendFile('index.html', {root: 'dist/ff13/'}),
-);
+  res.sendFile('index.html', {root: 'dist/ff13/'}));
 
 app.listen(process.env.PORT || 8080);
