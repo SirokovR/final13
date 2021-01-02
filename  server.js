5921c70594255cc72c6cd999ca990387101d1f3e
laisk <laisk1@hotmail.com>
2021-01-02 18:22:36 +0200

    deploy2.5

---  server.js
+++  server.js
@@ -1,16 +1,12 @@
-//Install express server
+
 const express = require('express');
-const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<ff13>'));
+app.use(express.static('./dist/ff13'));
 
-app.get('/*', function(req,res) {
+app.get('/*', (req, res) =>
+  res.sendFile('index.html', {root: 'dist/ff13/'}),
+);
 
-  res.sendFile(path.join(__dirname+'/dist/<ff13>/index.html'));
-});
-
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
